0c72dc250d4b389514047e6697b5500566760444
Michael Cote <michaelcote7@outlook.com>
2020-12-12 02:22:19 -0800

    Updates

---
+++ precache-manifest.93ddd5c6d2ab22af73a335d34c7a8472.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361314a6af959a0d444c237cfc1e4e62",
+    "url": "/jitsi-chat/index.html"
+  },
+  {
+    "revision": "1cb3a801ee84334b763c",
+    "url": "/jitsi-chat/static/css/2.59aa5021.chunk.css"
+  },
+  {
+    "revision": "9e45732c610a64aa47fe",
+    "url": "/jitsi-chat/static/css/main.e0cca709.chunk.css"
+  },
+  {
+    "revision": "1cb3a801ee84334b763c",
+    "url": "/jitsi-chat/static/js/2.efdccc29.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jitsi-chat/static/js/2.efdccc29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e45732c610a64aa47fe",
+    "url": "/jitsi-chat/static/js/main.a10d5352.chunk.js"
+  },
+  {
+    "revision": "be4aa1d4c65fa8973f02",
+    "url": "/jitsi-chat/static/js/runtime-main.3609a5d4.js"
+  },
+  {
+    "revision": "decaf47512dacac4189c825498221eec",
+    "url": "/jitsi-chat/static/media/bg_img.decaf475.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/jitsi-chat/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/jitsi-chat/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/jitsi-chat/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/jitsi-chat/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/jitsi-chat/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
