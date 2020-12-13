9b1b4c23bbfe9e891a792e67a68a98f939219e99
Michael Cote <michaelcote7@outlook.com>
2020-12-12 22:32:16 -0800

    .

--- src/App.js
+++ src/App.js
@@ -25,10 +25,10 @@ const App = () => {
   
   return (
     <div className="container-fluid main-container">
-      <div className="row my-4">
+      <div className={onCall?"row my-1":"row my-5"}>
         <div className="col">
           <div className="my-wrap">&#8943;&#8886;Hey there family&#8887;&#8943;</div>
-          <hr className="mb-5 mt-4 myHR"></hr>
+          <hr className={onCall?"my-1 myHR":"mt-5 mb-4 myHR"}></hr>
             <div className="jisti-container">
               { onCall ?
                 <Jitsi
@@ -37,9 +37,10 @@ const App = () => {
                   loadingComponent={Loader}
                   onAPILoad={JitsiMeetAPI => console.log('Good Morning everyone!')}
                   config={{prejoinPageEnabled: false, disableDeepLinking: true}}
+                  interfaceConfig={onclose = ()=>{console.log("CLOSED")}}
                 />
               :
-              <div className="my-wrap" style={{height: '380px'}}>
+              <div className="my-wrap" style={{height: '320px'}}>
                 <p className="my-ptag">Create / Join a Meeting</p>
                 <input className="my-input" type='text' placeholder='Room name' value={roomName} onChange={e => setRoomName(e.target.value)}/>
                 <input className="my-input" type='text' placeholder='Your name' value={displayName} onChange={e => setDisplayName(e.target.value)}/>
@@ -49,8 +50,8 @@ const App = () => {
               </div>
               }
             </div>
-          <hr className="mt-5 mb-4 myHR"></hr>
-          <div onClick={()=>setOnCall(false)}className="my-wrap p-1 pb-2 my-name">2021 michael cote &infin;&nbsp;{onCall?'hang-up':''}</div>
+          <hr className={onCall?"my-1 myHR":"mt-4 mb-5 myHR"}></hr>
+          <div onClick={()=>setOnCall(false)}className="my-wrap p-1 pb-2 my-name">2021 michael cote &infin;&nbsp;{onCall?'GO BACK':''}</div>
         </div>
       </div>
     </div>
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-
 import { css } from '@emotion/react';
-
 import GridLoader from "react-spinners/GridLoader";
-
 const Loader = () => {
-
   const override = css`
   margin: auto;
   -webkit-box-reflect: below 40px linear-gradient(
@@ -13,10 +9,8 @@ const Loader = () => {
     transparent,
     rgba(255, 255, 255, 0.4));
   `;
-
   return (
     <GridLoader color={'#ffffff80'} size={55} margin={5} css={override}/>
   );
 }
-
 export default Loader;
