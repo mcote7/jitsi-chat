751175ed7e9915c83ce457eef88d9d394db1d843
Michael Cote <michaelcote7@outlook.com>
2020-12-14 21:06:39 -0800

    .

--- src/utilities/connectBackgroundTransition.js
+++ src/utilities/connectBackgroundTransition.js
@@ -4,11 +4,11 @@ export const connectBackgroundTransitionGroup = {
 
   onConnect: () => {
     const main_BG = CSSRulePlugin.getRule(".main-container:before");
-    TweenLite.to(main_BG, 2, {cssRule:{opacity: 0.9, scale: 1.25, rotate: 7, filter: "blur(12px)"}});
+    TweenLite.to(main_BG, 2, {cssRule:{opacity: 1, scale: 1.25, rotate: 7, filter: "blur(12px)"}});
   },
   onDisconnect: () => {
     const main_BG = CSSRulePlugin.getRule(".main-container:before");
-    TweenLite.to(main_BG, 2, {cssRule:{opacity: 0.45, scale: 1, rotate: -7, filter: "blur(0px)"}});
+    TweenLite.to(main_BG, 2, {cssRule:{opacity: 0.6, scale: 1, rotate: -7, filter: "blur(0px)"}});
   }
 
 };
