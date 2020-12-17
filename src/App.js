4c70f7bb6d24a5800fc5972210eb2bc59523d3b0
Michael Cote <michaelcote7@outlook.com>
2020-12-16 17:56:14 -0800

    .

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 
 import './SASS/main.scss';
 
+import { connectBackgroundTransitionGroup } from './utilities/connectBackgroundTransition';
+
 import Loader from './components/Loader';
 
 import Jitsi from 'react-jitsi';
 
-import { connectBackgroundTransitionGroup } from './utilities/connectBackgroundTransition';
 
 const App = () => {
 
@@ -27,7 +28,7 @@ const App = () => {
     <div className="container-fluid main-container">
       <div className={onCall?"row my-1":"row my-5"}>
         <div className="col">
-          <div className="my-wrap">&#8943;&#8886;Hey there family&#8887;&#8943;</div>
+          <div className="my-wrap">&#8943;&#8886;&nbsp;Cote's Chat&nbsp;&#8887;&#8943;</div>
           <hr className={onCall?"my-1 myHR":"mt-5 mb-4 myHR"}></hr>
             <div className="jisti-container">
               { onCall ?
@@ -40,7 +41,7 @@ const App = () => {
                 />
               :
               <div className="my-wrap" style={{height: '320px'}}>
-                <p className="my-ptag">Create / Join a Meeting</p>
+                <p className="my-ptag">Create / Join a Room</p>
                 <input className="my-input" type='text' placeholder='Room name' value={roomName} onChange={e => setRoomName(e.target.value)}/>
                 <input className="my-input" type='text' placeholder='Your name' value={displayName} onChange={e => setDisplayName(e.target.value)}/>
                 <button className="my-button" onClick={() => setOnCall(true)}>
